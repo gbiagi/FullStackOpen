893f839d77dc814c31dcc5d69ea1f037ae9a429d
Geanfranco Biagi <118919877+gbiagi@users.noreply.github.com>
2026-02-28 23:50:45 +0100

    pt2ex2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,28 +1,42 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault() // prevents reload
     const names = persons.map((person) => person.name)
+
+    if (newName === '' || newNumber === '') {
+      console.log("Empty fields error")
+      alert("Fields cannot be empty")
+      return
+    }
+
     if (names.includes(newName)) {
-      console.log("Name repeated")
+      console.log("Name repeated error")
       alert(`${newName} is already added to phonebook`)
       return
     }
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
   const handleNameChanger = (event) => {
     // console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChanger = (event) => {
+    // console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
   return (
     <div>
@@ -30,16 +44,18 @@ const App = () => {
       <form>
         <div>
           name: <input onChange={handleNameChanger} value={newName} />
+          <br />
+          number: <input onChange={handleNumberChanger} value={newNumber} />
         </div>
         <div>
-          <button onClick={addName} type="submit">add</button>
+          <button onClick={addPerson} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
           <li key={person.name}>
-            {person.name}
+            {person.name} {person.number}
           </li>
         ))}
       </div>

bcc4874bb4bb80ccc9e435a8847eb8be48cdf22d
Geanfranco Biagi <118919877+gbiagi@users.noreply.github.com>
2026-05-18 01:34:17 +0200

    pt4ex4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,6 +4,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+sumTotalLikes = (blogs) => {
+    let total = 0
+    blogs.forEach(blog => {
+        total = total + blog.likes
+    });
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    sumTotalLikes
 }
